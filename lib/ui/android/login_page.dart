f0ab2e425f50af88432293f0e6958b8e42c71690
rayenealmeida <rayene@devartbots.com.br>
2022-05-06 17:12:07 -0300

    commit17

--- lib/ui/android/login_page.dart
+++ lib/ui/android/login_page.dart
@@ -5,10 +5,35 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(title: Text('LoginPage')),
-    body: Container(
-      child: Text('LOGIN PAGE'),
-    ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+            shrinkWrap: true,
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            children: [
+              Hero(
+                tag: 'hero', 
+                child: CircleAvatar(
+                  backgroundColor: Colors.transparent,
+                  radius: 48.0,
+                ),
+              ),
+              SizedBox(height: 48.0),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                autofocus: false,
+                initialValue: "emailteste@gmail.com",
+                decoration: InputDecoration(
+                  hintText: "E-mail",
+                  contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32.0), 
+                )
+              ),
+            )
+          ],
+        )
+      )
     );
   }
 }
