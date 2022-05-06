2fbd9c8c2b61e5b1a8a8a79607cf1b92d9fde007
rayenealmeida <rayene@devartbots.com.br>
2022-05-06 16:20:33 -0300

    commit

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -2,11 +2,14 @@
 
   import 'package:get/get.dart';
 import 'package:login/routes/app_routes.dart';
+import 'package:login/ui/android/home_page.dart';
+import 'package:login/ui/android/initial_page.dart';
+import 'package:login/ui/android/login_page.dart';
 
 class AppPages{
     static final routes =[
-      GetPage(name: Routes.INITIAL, page: () => null),
-      GetPage(name: Routes.LOGIN, page: () => null),
-      GetPage(name: Routes.HOME, page: () => null),
+      GetPage(name: Routes.INITIAL, page: () => InitialPage(),),
+      GetPage(name: Routes.LOGIN, page: () => LoginPage(),),
+      GetPage(name: Routes.HOME, page: () => HomePage(),),
     ];
   }
