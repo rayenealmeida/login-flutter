85a968ca92a9fb989b112ace3a0931e9b22a2133
rayenealmeida <rayene@devartbots.com.br>
2022-05-06 16:06:06 -0300

    android

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:get/route_manager.dart';
+import 'package:login/routes/app_pages.dart';
+import 'package:login/routes/app_routes.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
       title: 'Cadastro de usuário',
+      debugShowCheckedModeBanner: false,
+      getPages: AppPages.routes,
+      initialRoute: Routes.INITIAL,
       home: Scaffold(appBar: AppBar(title: Text("Login"),),),
     )
   );
---
+++ lib/ui/android/initial_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class InitialPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(title: Text('InitialPage')),
+    body: Container(
+      child: Container(),
+    
+      ),
+    );
+  }
+}
