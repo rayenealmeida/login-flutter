75af58acd740cb8a15760192577c99084b6e28c0
rayenealmeida <rayene@devartbots.com.br>
2022-05-06 16:27:23 -0300

    theme

---
+++ lib/ui/theme/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appThemeData = ThemeData(
+  primaryColor: Colors.pink,
+  brightness: Brightness.light,
+  accentColor: Colors.green[50],
+  appBarTheme: AppBarTheme(
+    color: Colors.green,
+  ),
+);
