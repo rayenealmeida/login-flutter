508d1c5c653014e9f33ee852c9fdf9fd17e56d74
rayenealmeida <rayene@devartbots.com.br>
2022-05-06 17:56:12 -0300

    commit

--- lib/ui/android/initial_page.dart
+++ lib/ui/android/initial_page.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 class InitialPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
     appBar: AppBar(title: Text('InitialPage')),
-    body: Container(
-      child: Text("INITIAL"),
-      ),
+    body: Stack(
+      children: [
+        SplashScreen(
+
+        ),
+      ],
+
+    ),
     );
   }
 }
