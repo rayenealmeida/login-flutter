e82344dcc081a60d6f9ac76f7a840ccb8ff161e9
rayenealmeida <rayene@devartbots.com.br>
2022-05-09 16:28:06 -0300

    key

--- lib/ui/android/home_page.dart
+++ lib/ui/android/home_page.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage ({Key? key}):super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(title: Text('HomePage')),
+    appBar: AppBar(title: const Text("HomePage")),
     body: Container(
       child: Text("HOME"),
     ),
