f41e013184d3cc2ffee81cc59ed0d4ed2bad72bd
rayenealmeida <rayene@devartbots.com.br>
2022-05-09 16:28:10 -0300

    key

--- lib/ui/android/login_page.dart
+++ lib/ui/android/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage ({Key? key}):super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
