46be8eae4278ec78069fd1f970adb143d4e38b1a
rayenealmeida <rayene@devartbots.com.br>
2022-05-09 16:41:33 -0300

    const

--- lib/ui/android/login_page.dart
+++ lib/ui/android/login_page.dart
@@ -10,58 +10,58 @@ class LoginPage extends StatelessWidget {
       body: Center(
         child: ListView(
             shrinkWrap: true,
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
             children: [
-              Hero(
+              const Hero(
                 tag: 'hero', 
                 child: CircleAvatar(
                   backgroundColor: Colors.transparent,
                   radius: 48.0,
                 ),
               ),
-              SizedBox(height: 48.0),
+              const SizedBox(height: 48.0),
               TextFormField(
                 keyboardType: TextInputType.emailAddress,
                 autofocus: false,
                 initialValue: "emailteste@gmail.com",
                 decoration: InputDecoration(
                   hintText: "E-mail",
-                  contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(32.0), 
                 )
               ),
             ),
-            SizedBox(height: 8.0),
+            const SizedBox(height: 8.0),
             TextFormField(
                 autofocus: false,
                 //initialValue: "password",
                 obscureText: true,
                 decoration: InputDecoration(
                   hintText: "Senha",
-                  contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(32.0),
                 )
               ),
             ),
-            SizedBox(height: 24.0,),
+            const SizedBox(height: 24.0,),
             Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: RaisedButton(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(24.0),
                 ),
                 onPressed: () { },
-                padding: EdgeInsets.all(12.0),
+                padding: const EdgeInsets.all(12.0),
                 color: Get.theme.primaryColor,
-                child: Text("Acessar", style: TextStyle(color: Colors.white),),
+                child: const Text("Acessar", style: TextStyle(color: Colors.white),),
               ),
             ),
               FlatButton(
                 onPressed: () { },
-                child: Text("Esqueceu a senha?", style: TextStyle(color: Colors.black54),),
-              ),
+                child: const Text("Esqueceu a senha?", style: TextStyle(color: Colors.black54),),
+            ),
           ],
         )
       )
